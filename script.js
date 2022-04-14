258e70d1074a38b0302ea37a90d1df2c9278cb2c
Todd Stuke <tddstuke@gmail.com>
2022-04-13 19:48:17 -0500

    added current date to title

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // add moment.js or day.js to title
-
-// make timeblock divs editable
+var today = dayjs();
+console.log(today.format("dddd, MMMM D, YYYY h:mm A"));
+var getDay = function () {
+  $("#currentDay").text(today.format("dddd, MMMM, YYYY"));
+};
+getDay();
+// make timeblock divs editable when clicked on
 
 // make save function tied to buttons
 
-// add color codes time-blocks for time of day
+// add color codes time-blocks for time of da
