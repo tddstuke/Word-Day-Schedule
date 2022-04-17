56beaed51948ab94bd3facca5b8d136a4190e63b
Todd Stuke <tddstuke@gmail.com>
2022-04-17 10:26:52 -0500

    commented console.logs out of code

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var tasks = [];
 // add moment.js or day.js to title
 
 var today = dayjs();
-console.log(today.format("dddd, MMMM D, YYYY h:mm A"));
 var getDay = function () {
   $("#currentDay").text(today.format("dddd, MMMM D, YYYY"));
 };
@@ -25,8 +24,7 @@ $(".saveBtn").on("click", function () {
   var text = $(this).siblings("textarea").val().trim();
 
   var hour = $(this).siblings(".time").attr("id");
-
-  console.log(hour);
+  // console.log(hour);
   var taskP = $("<p>").addClass("col-10 time text-left m-0 pt-2").text(text);
 
   $(this).siblings("textarea").replaceWith(taskP);
@@ -35,13 +33,11 @@ $(".saveBtn").on("click", function () {
   auditTask(taskP);
 });
 
-var createTask = function () {};
-
 // make save function tied to buttons
 var loadTask = function (taskEl) {
   var hour = $(taskEl).attr("id");
   var text = localStorage.getItem(hour);
-  console.log(text);
+  // console.log(text);
 
   $(taskEl).text(text);
 };
@@ -50,10 +46,10 @@ var loadTask = function (taskEl) {
 var auditTask = function (taskEl) {
   // get time from task element
   var hour = parseInt($(taskEl).attr("id"));
-  console.log(hour);
+  // console.log(hour);
   // convert to a dayjs object
   var time = dayjs().hour();
-  console.log(time);
+  // console.log(time);
 
   // apply new class if task is near/over due date
   if (hour < time) {
@@ -71,13 +67,11 @@ var saveTask = function (hour, text) {
 setInterval(function () {
   $(".time").each(function (index, el) {
     auditTask(el);
-    console.log("timer");
+    // console.log("timer");
   });
 }, 1000 * 60);
 
 $(".row").on("click", ".time", editBlock);
-
-// loadTask();
 
 $(".time").each(function (index, el) {
   auditTask(el);
